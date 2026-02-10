8c850b070bdff0a01304cb59a003c252e48f5ec3
Vova Lysakov <vladimir.lysakov2006@gmain.com>
2026-02-10 10:28:29 +0000

    Добавил БД SQLite

--- server.py
+++ server.py
@@ -1,11 +1,51 @@
-import hmac, hashlib, time, json
-from fastapi import FastAPI, HTTPException
-import random
-from pydantic import BaseModel
-from urllib.parse import parse_qsl
+import os  # 1. Импортируем модуль для работы с системой
+from dotenv import load_dotenv # 2. Импортируем загрузчик переменных окружения из файла .env
+load_dotenv()  # 3. Загружаем переменные окружения из файла
+
+import sqlite3 # 4. Импортируем модуль для работы с базой данных SQLite
+
+import hmac, hashlib, time, json # 5. Импортируем модули для работы с криптографией, временем и JSON
+from fastapi import FastAPI, HTTPException 
+import random 
+from pydantic import BaseModel 
+from urllib.parse import parse_qsl 
+
+
+
+DB_NAME = "bot_database.db"
+
+def init_db(): # Инициализируем базу данных, создавая таблицу для хранения данных пользователей, если она еще не существует
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER PRIMARY KEY,
+            last_number INTEGER
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def save_user_number(user_id: int, number: int): # Сохраняем сгенерированное число для пользователя
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute('INSERT OR REPLACE INTO users (user_id, last_number) VALUES (?, ?)', (user_id, number))
+    conn.commit()
+    conn.close()
+
+def get_user_number(user_id: int): # Получаем последнее сохраненное число для пользователя
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute('SELECT last_number FROM users WHERE user_id = ?', (user_id,))
+    result = cursor.fetchone() # Вернет кортеж (число,) или None
+    conn.close()
+    return result[0] if result else None
+
+init_db()
+
 
 app = FastAPI()
-BOT_TOKEN = "7040597128:AAE2kqIGgmH0uZAhQpZlaq38XQnRJAXNr38"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 class AuthReq(BaseModel):
     initData: str
@@ -35,22 +75,31 @@ def auth(req: AuthReq):
     user = json.loads(data["user"])
     user_id = user["id"]
 
-    # тут ты можешь загрузить профиль из БД
-    balance = 100  # пример
+    
 
-    return {"user_id": user_id, "balance": balance}
+    balance = get_user_number(user_id)
+    if (balance is None):
+        return {"user_id": user_id, "last_number": "нет данных"}
+    return {"user_id": user_id, "last_number": balance}
 
 class EchoReq(BaseModel):
     value: str
+
+
+
 class RangeReq(BaseModel):
+    user_id: int
     min: int
     max: int
-
 
 @app.post("/number")
 def number_range(req: RangeReq):
     if req.min > req.max:
         return {"error": "min > max"}
 
+
     n = random.randint(req.min, req.max)
+
+    save_user_number(req.user_id, n)
+
     return {"number": n}
